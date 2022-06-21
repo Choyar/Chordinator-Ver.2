348cf4b7e621f3f02dbca982e000d3f52722ae22
choyar <choyar70407@gmail.com>
2022-06-21 20:30:46 +0800

    Create melody input interface

--- Chordinator ver.2/Form1.Designer.cs
+++ Chordinator ver.2/Form1.Designer.cs
@@ -28,13 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.vsbSample = new System.Windows.Forms.VScrollBar();
+            this.picboxSample = new System.Windows.Forms.PictureBox();
+            this.vsbarSample2 = new System.Windows.Forms.VScrollBar();
+            this.picboxSample2 = new System.Windows.Forms.PictureBox();
+            this.labelSample = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxSample)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxSample2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(24, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(78, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.numericUpDown1.Location = new System.Drawing.Point(64, 83);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(52, 39);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // vsbSample
+            // 
+            this.vsbSample.LargeChange = 1;
+            this.vsbSample.Location = new System.Drawing.Point(122, 44);
+            this.vsbSample.Maximum = 17;
+            this.vsbSample.Name = "vsbSample";
+            this.vsbSample.Size = new System.Drawing.Size(45, 73);
+            this.vsbSample.TabIndex = 2;
+            this.vsbSample.Visible = false;
+            // 
+            // picboxSample
+            // 
+            this.picboxSample.Location = new System.Drawing.Point(122, 59);
+            this.picboxSample.Name = "picboxSample";
+            this.picboxSample.Size = new System.Drawing.Size(45, 43);
+            this.picboxSample.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picboxSample.TabIndex = 3;
+            this.picboxSample.TabStop = false;
+            this.picboxSample.Visible = false;
+            // 
+            // vsbarSample2
+            // 
+            this.vsbarSample2.LargeChange = 1;
+            this.vsbarSample2.Location = new System.Drawing.Point(170, 44);
+            this.vsbarSample2.Maximum = 17;
+            this.vsbarSample2.Name = "vsbarSample2";
+            this.vsbarSample2.Size = new System.Drawing.Size(45, 73);
+            this.vsbarSample2.TabIndex = 2;
+            this.vsbarSample2.Visible = false;
+            // 
+            // picboxSample2
+            // 
+            this.picboxSample2.Location = new System.Drawing.Point(170, 59);
+            this.picboxSample2.Name = "picboxSample2";
+            this.picboxSample2.Size = new System.Drawing.Size(45, 43);
+            this.picboxSample2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picboxSample2.TabIndex = 3;
+            this.picboxSample2.TabStop = false;
+            this.picboxSample2.Visible = false;
+            // 
+            // labelSample
+            // 
+            this.labelSample.Font = new System.Drawing.Font("新細明體", 40F);
+            this.labelSample.Location = new System.Drawing.Point(218, 44);
+            this.labelSample.Name = "labelSample";
+            this.labelSample.Size = new System.Drawing.Size(45, 73);
+            this.labelSample.TabIndex = 4;
+            this.labelSample.Text = "|";
+            this.labelSample.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelSample.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.button1.Location = new System.Drawing.Point(269, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 36);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "+";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("新細明體", 20F);
+            this.button2.Location = new System.Drawing.Point(269, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 36);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "-";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1256, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelSample);
+            this.Controls.Add(this.picboxSample2);
+            this.Controls.Add(this.picboxSample);
+            this.Controls.Add(this.vsbarSample2);
+            this.Controls.Add(this.vsbSample);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Chordinator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxSample)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxSample2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.VScrollBar vsbSample;
+        private System.Windows.Forms.PictureBox picboxSample;
+        private System.Windows.Forms.VScrollBar vsbarSample2;
+        private System.Windows.Forms.PictureBox picboxSample2;
+        private System.Windows.Forms.Label labelSample;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Chordinator ver.2/Form1.cs
+++ Chordinator ver.2/Form1.cs
@@ -16,5 +16,165 @@ namespace Chordinator_ver._2
         {
             InitializeComponent();
         }
+        
+        
+        class note
+        {
+            Form1 f;
+            PictureBox picbox;
+            VScrollBar vsbar;
+            Bitmap[] scaleImgs;
+            String[] scaleStrs = new string[]
+            {"rest","line","C","C sharp","D", "D sharp", "E", "F", "F sharp", "G", "G sharp", "A", "A sharp","B" ,"higher C"};
+            Point location;
+            public void SetLocation(int x,int y)
+            {
+                location.X = x;
+                location.Y = y;
+                vsbar.Location = location;
+                int yoff = f.picboxSample.Location.Y - f.vsbSample.Location.Y;
+                picbox.Location = new Point(location.X, location.Y + yoff);
+            }
+            public note(Form1 form)
+            {
+                f = form;
+                location = new Point(f.vsbSample.Location.X, f.vsbSample.Location.Y);
+                vsbar = new VScrollBar();
+                vsbar.Size = f.vsbSample.Size;
+                vsbar.Location = f.vsbSample.Location;
+                vsbar.LargeChange = 1;
+                picbox = new PictureBox();
+                picbox.Size = f.picboxSample.Size;
+                picbox.Location = f.picboxSample.Location;
+                picbox.SizeMode = f.picboxSample.SizeMode;
+                f.Controls.Add(vsbar);
+                f.Controls.Add(picbox);
+                picbox.BringToFront();
+
+                SetImageArray();
+                picbox.Image = scaleImgs[0];
+                vsbar.Maximum = scaleStrs.Length - 1;
+                vsbar.Value = vsbar.Maximum;
+                vsbar.Minimum = 0;
+
+                vsbar.Scroll += Vsbar_Scroll;
+            }
+
+            private void Vsbar_Scroll(object sender, ScrollEventArgs e)
+            {
+                int val = ((VScrollBar)sender).Maximum - ((VScrollBar)sender).Value;
+                picbox.Image = scaleImgs[val];
+            }
+
+            private void SetImageArray()
+            {
+                scaleImgs = new Bitmap[scaleStrs.Length];
+                for (int a = 0; a < scaleStrs.Length; a++)
+                {
+                    scaleImgs[a] = new Bitmap($"{scaleStrs[a]}.png");
+                }
+            }
+
+            public void remove()
+            {
+                try
+                {
+                    f.Controls.Remove(picbox);
+                    f.Controls.Remove(vsbar);
+                }
+                catch { }
+            }
+
+        }
+
+        List<note> melody = new List<note>();
+        List<Label> bars = new List<Label>();
+        int bps;
+        int offsetX;
+        Point startLocation;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            startLocation = vsbSample.Location;
+            offsetX = picboxSample2.Location.X - picboxSample.Location.X;
+            bps = (int)numericUpDown1.Value;
+            for(int a=0;a<bps;a++)
+            {
+                melody.Add(new note(this));
+            }
+            ArrangeMelody();
+        }
+        private void ArrangeMelody()
+        {
+            ClearBars();
+            while(melody.Count%bps!=0)
+            {
+                melody.Add(new note(this));
+            }
+            int section = 0;
+            for(int a=0;a<melody.Count;a++)
+            {
+                if(a!=0&&a%bps==0)
+                {
+                    setbar(a+section);
+                    section++;
+                }
+                melody[a].SetLocation(startLocation.X + offsetX * (a + section),startLocation.Y);
+            }
+            setbar(melody.Count + section);
+            bars.Last().Text = "||";
+            button1.Location = new Point(startLocation.X + offsetX * (melody.Count + section + 1), startLocation.Y);
+            button2.Location = new Point(startLocation.X + offsetX * (melody.Count + section + 1), startLocation.Y+button1.Height+1);
+            if(melody.Count>bps)
+            {
+                button2.Enabled = true;
+            }
+            else
+            {
+                button2.Enabled = false;
+            }
+        }
+        private void ClearBars()
+        {
+            foreach(Label l in bars)
+            {
+                Controls.Remove(l);
+            }
+            bars.Clear();
+        }
+        private void setbar(int num)
+        {
+            Label l = new Label();
+            l.Text = "|";
+            l.AutoSize = false;
+            l.Font = labelSample.Font;
+            l.Size = labelSample.Size;
+            l.Location = new Point(startLocation.X + offsetX * num, startLocation.Y);
+            l.TextAlign = labelSample.TextAlign;
+            Controls.Add(l);
+            bars.Add(l);
+        }
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            bps = (int)numericUpDown1.Value;
+            ArrangeMelody();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            for(int a=0;a<bps;a++)
+            {
+                melody.Add(new note(this));
+            }
+            ArrangeMelody();
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            for(int a = 0; a < bps; a++)
+            {
+                melody.Last().remove();
+                melody.RemoveAt(melody.Count - 1);
+            }
+            ArrangeMelody();
+        }
     }
 }
